509c6bfa6020f5c2c9fe4b6c99275652b43a46fc
Olavi Haapala <mail@olavihaapala.fi>
2023-06-20 02:58:32 +0300

    Fix the logic

--- backend/db.js
+++ backend/db.js
@@ -18,21 +18,19 @@ async function getResultsForScreenName(screenName) {
   return collection.find({}, { name: 1, _id: 0 }).toArray();
 }
 
-async function getPreviousResultForScreenName(screenName) {
-  return new Promise((resolve, reject) => {
-    db.collection(`results_${screenName}`)
-      .find({}, { name: 1, _id: 0 })
-      .sort({ _id: -1 })
-      .limit(1)
-      .toArray(function(err, data) {
-        if (err ? reject(err) : resolve(data[0]));
-      });
-  });
+async function getPreviousResultForScreenName(collection) {
+  return collection
+    .find({}, { name: 1, _id: 0 })
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray(function(err, data) {
+      if (err ? reject(err) : resolve(data[0]));
+    });
 }
 
 async function insertAndTweetIfChanged(screenName, result, tweetNowFn) {
   const collection = db.collection(`results_${screenName}`);
-  getPreviousResultForScreenName(screenName).then(previousResult => {
+  await getPreviousResultForScreenName(collection).then(previousResult => {
     console.log('Found from db:', { previousResult, result });
 
     if (!previousResult || (previousResult && previousResult.name !== result)) {
