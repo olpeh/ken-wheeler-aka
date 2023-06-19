712cd9f4ff361e4d3fed23b35bbb48d5530a1ffc
Olavi Haapala <mail@olavihaapala.fi>
2023-06-20 01:00:01 +0300

    Remove possibly unneeded config

--- backend/config.js
+++ backend/config.js
@@ -8,14 +8,7 @@ module.exports = {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   },
   twitterConfig: {
-    queryString: process.env.QUERY_STRING,
-    resultType: process.env.RESULT_TYPE,
-    language: process.env.TWITTER_LANG,
-    username: process.env.TWITTER_USERNAME,
     check: process.env.TWITTER_CHECK_RATE * 1000 * 60,
-    minInterval: process.env.TWITTER_MIN_INTERVAL * 1000 * 60,
-    searchCount: process.env.TWITTER_SEARCH_COUNT,
-    randomReply: process.env.RANDOM_REPLY,
     enabled: process.env.TWEETING_ENABLED || false
   }
 };
